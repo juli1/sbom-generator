d91e32f6b2ab4b9c0be18330c8cb93369a18bc0f
Julien Delange <julien.delange@datadoghq.com>
2024-10-16 14:58:03 -0400

    update

--- misc/compare-sboms.sh
+++ misc/compare-sboms.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 DIRECTORY=$1
-EXPECTED_NUMBER_ERRORS=$2
+EXPECTED_NUMBER_ERRORS_OSV_SCANNER=$2
+EXPECTED_NUMBER_ERRORS_TRIVY=$3
 TEST_DIR=$(mktemp -d)
 
 cargo build -r
@@ -10,12 +11,21 @@ echo "testing on ${DIRECTORY}"
 echo "test dir on ${TEST_DIR}"
 
 osv-scanner  --skip-git -r --experimental-only-packages --format=cyclonedx-1-5 --paths-relative-to-scan-dir --output "${TEST_DIR}/osv-scanner.json" "${DIRECTORY}"
+trivy fs --output "${TEST_DIR}/trivy.json" --format cyclonedx "${DIRECTORY}"
 ./target/release/sbom-generator --directory "${DIRECTORY}" --output "${TEST_DIR}/sbom-generator.json"
 
 misc/compare-sbom.py "${TEST_DIR}/osv-scanner.json" "${TEST_DIR}/sbom-generator.json"
 ACTUAL_NUMBER_ERRORS=$?
-if [ "${ACTUAL_NUMBER_ERRORS}" != "${EXPECTED_NUMBER_ERRORS}" ]; then
-  echo "Number of errors mismatch. Expected ${EXPECTED_NUMBER_ERRORS}, got ${ACTUAL_NUMBER_ERRORS}"
+if [ "${ACTUAL_NUMBER_ERRORS}" != "${EXPECTED_NUMBER_ERRORS_OSV_SCANNER}" ]; then
+  echo "[osv-scanner] number of errors mismatch. Expected ${EXPECTED_NUMBER_ERRORS_OSV_SCANNER}, got ${ACTUAL_NUMBER_ERRORS}"
   exit 1
 fi
+
+misc/compare-sbom.py "${TEST_DIR}/trivy.json" "${TEST_DIR}/sbom-generator.json"
+ACTUAL_NUMBER_ERRORS=$?
+if [ "${ACTUAL_NUMBER_ERRORS}" != "${EXPECTED_NUMBER_ERRORS_TRIVY}" ]; then
+  echo "[trivy] number of errors mismatch. Expected ${EXPECTED_NUMBER_ERRORS_TRIVY}, got ${ACTUAL_NUMBER_ERRORS}"
+  exit 1
+fi
+
 exit 0
